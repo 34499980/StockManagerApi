cfd7988970636434627c40d37eb95017e3086015
34499980 <aribrenman@hotmail.com>
2020-08-25 11:39:20 -0300

    Creacion constants

---
+++ ConstantControl/Constants.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConstantControl
+{
+    public class Constants
+    {
+
+        public enum Dispatch_State
+        {
+            Creado = 1,
+            Despachado = 2,
+            Recibido = 3,
+            Finalizado = 4,
+            Incompleto = 5
+            
+        }
+
+        public enum Sale_State
+        {
+            Inicial = 1,
+            Reservado = 2,
+            Finalizado = 3,
+            Devuelto = 4
+           
+
+        }
+        public enum Stock_State
+        {
+            Habilitado = 1,
+            Deshabilitado = 2
+          
+
+
+        }
+
+    }
+}
