fd6e10af47ffd14ed720a97a22dfba9d1494c101
Ariel <aribrenman@hotmail.com>
2020-11-28 18:57:26 -0300

    change dispatch type

--- StockManagerApi/Controllers/DispatchController.cs
+++ StockManagerApi/Controllers/DispatchController.cs
@@ -38,7 +38,7 @@ namespace StockManagerApi.Controllers
 
         // GET api/<DispatchController>/5
         [HttpGet("{id}")]
-        public IEnumerable<DispatchDto> Get(string id)
+        public IEnumerable<DispatchDto> Get(int id)
         {
             try
             {
