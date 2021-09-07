c01bc5a60a9cab69cfba9ad8620dd21c69be5ef0
NOTEBOOKDELL\Dell <aribrenman@hotmail.com>
2021-09-07 19:57:30 -0300

    add historydto

---
+++ DTO/Class/HistoryDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace DTO.Class
+{
+  
+    public class HistoryDto
+    {
+
+        public int ID { get; set; }
+        public DateTime DateProces { get; set; }
+        public int IdUser { get; set; }
+        public string Action { get; set; }
+        public string ActionDetail { get; set; }
+        public string Refer { get; set; }
+
+        public  UserDto User{ get; set; }
+      
+     
+
+    }
+}
---
+++ Repository/Entities/History.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Repository.Entities
+{
+    [Table("HISTORY", Schema = "dbo")]
+    public class History
+    {
+
+        [Key]
+        public int ID { get; set; }
+        public DateTime DateProces { get; set; }
+        public int IdUser { get; set; }
+        public string Action { get; set; }
+        public string ActionDetail { get; set; }
+        public string Refer { get; set; }       
+
+
+        [ForeignKey("IdUser")]
+        public virtual User User { get; set; }
+      
+
+    }
+}
